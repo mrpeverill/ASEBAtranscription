5c0b7f901ae527d7a37d03ae02c5896c6e032417
peverill <mrpeverill@gmail.com>
2022-11-14 13:03:46 -0600

    Renamed Files

---
+++ AsebaParseLib.py
@@ -0,0 +1,187 @@
+import datetime
+import warnings
+import pprint
+import uuid
+
+def asebadictformat(mapping,cbcldat,ysrdat=None):
+    # print("cbclisportstimea is %s" % int(cbcldat['cbclisportstimea']))
+    # print("sportstimea is %s" % arithcode(-1,'cbclisportstimea',cbcldat))
+    ppf = pprint.PrettyPrinter(indent=4)
+
+    #ppf.pprint(mapping['3020']['SourceField'])
+    #ppf.pprint(arithcode(-1,'cbclfuisportsabilitya',cbcldat))
+
+    #ppf.pprint(mapping[[key for key,value in mapping.items() if key.startswith("3")]])
+    #ppf.pprint([getmappings(i,ysrdat,mapping) for i in mapping if i[:1]=="5"])
+
+    recordid=getmappings('1001',cbcldat,mapping)['Value']
+    age=getmappings(3005,cbcldat,mapping)['Value']
+    gender=getmappings(3004,cbcldat,mapping)['Value']
+    genderstring=genderrecode(gender,recordid)
+
+    try:
+        if int(age) < 6 or int(age) > 18:
+            warnings.warn("Record %s age out of range" % recordid,UserWarning)
+    except TypeError:
+        warnings.warn("Record %s age improperly formatted" % recordid,UserWarning)
+    except ValueError:
+        warnings.warn("Record %s age missing or misformatted" % recordid,UserWarning)
+
+    formslist=[cbcldictformat(cbcldat,mapping)]
+    if ysrdat!=None:
+        #print('appending ysr data')
+        formslist.insert(0,ysrdictformat(ysrdat,mapping,gender,age))
+    #pp.pprint(ysrdictformat(ysrdat))
+
+
+    dob=getmappings(3009,cbcldat,mapping)['Value']
+    dict={'DateOfBirth': dob+'T00:00:00',
+        'Ethnicity': '',
+        'Forms': formslist,
+        'Gender': genderstring,
+        'PersonInformation': {   'ContactInfo': {   'Address': {   'City': '',
+                                                                   'Country': '',
+                                                                   'PostalCode': '',
+                                                                   'StateProvince': '',
+                                                                   'Street1': '',
+                                                                   'Street2': ''},
+                                                    'Email': '',
+                                                    'HomePhone': '',
+                                                    'MobilePhone': '',
+                                                    'WorkPhone': ''},
+                                 'First': '',
+                                 'IdentificationCode': recordid,
+                                 'Last': '',
+                                 'Middle': '',
+                                 'NickName': '',
+                                 'Title': ''}}
+
+    return dict;
+
+def ysrdictformat(dat,mapping,gender,age):
+    #Answerslist=[getmappings(i) for i in mapping if ]
+    dict = {   'Agency': '',
+                         'AsebaDataId': str(uuid.uuid4()),
+                         'Clinician': '',
+                         'ContactEmail': '',
+                         'ContactName': '',
+                         'ContactPhone': '',
+                         'EvaluationId': '',
+                         'FormInstrument': {   'Answers': [{"Comment": "", "QuestionId": 5004, "Value": gender},
+                                                           {"Comment": "", "QuestionId": 5005, "Value": age}] +
+                                               [getmappings(i,dat,mapping) for i in mapping if i[:1]=="5"],# All the YSR id's start with 5
+                         "Id": "69e3fe20-0206-4a5b-a62e-abcaed7aab79"},
+                         'Relationship': 'Self',
+                         'School': '',
+                         'Society': {'Id': '622adb6d-dede-48e3-ae34-e80cdeb37ed8'},
+                         'Status': 'Key-Entered',
+                         'UserDefined1': '',
+                         'UserDefined2': '',
+                         'UserDefinedText1': '',
+                         'UserDefinedText2': ''}
+    dict['FormInstrument']['Answers']=removeEmptyValues(dict['FormInstrument']['Answers'])
+    return dict;
+
+def cbcldictformat(dat,mapping):
+    dict={   'Agency': '',
+                         'AsebaDataId': '5835f083-d874-2853-971b-d8b591bd6874',
+                         'Clinician': '',
+                         'ContactEmail': '',
+                         'ContactName': '',
+                         'ContactPhone': '',
+                         'EvaluationId': '',
+                         'FormInstrument': {   'Answers': [getmappings(i,dat,mapping) for i in mapping if i[:1]=="3"],
+                                               'Id': '07855877-88cf-458a-8d4e-93be0af21fa6'},
+                         'Relationship': '', #currently not coded
+                         'School': '',
+                         'Society': {'Id': '622adb6d-dede-48e3-ae34-e80cdeb37ed8'},
+                         'Status': 'Key-Entered',
+                         'UserDefined1': '',
+                         'UserDefined2': '',
+                         'UserDefinedText1': '',
+                         'UserDefinedText2': ''}
+    dict['FormInstrument']['Answers']=removeEmptyValues(dict['FormInstrument']['Answers'])
+    return dict;
+
+def genderrecode(gender,record):
+    if gender=='1':
+        return 'M'
+    elif gender=='2':
+        return 'F'
+    else:
+        warnings.warn("Record %s gender unset in CBCL, setting to 'U'" % record,UserWarning)
+        return 'U'
+
+def arithcode(y,string,dat):
+    try:
+        x=str(int(dat[string])+y)
+    except ValueError:
+        x=''
+    except TypeError:
+        x=''
+    except KeyError:
+        x=''
+    return x
+
+def removeEmptyValues(listarg):
+    return [item for item in listarg if item['Value']!='' and item['Value']!='999' and item['Value']!='NA']
+
+
+def getmappings(id,dat,mapping):
+    #What columns are we looking at in dat for comment and value?
+    comment=mapping[str(id)]['SourceCommentField']
+    value=mapping[str(id)]['SourceField']
+    #E.g. comment could be "dat['cbcl_cage']"
+    
+    if comment:
+        try:
+            commentv=eval(comment) # E.g. evaluate dat['cbcl_cage'] to get that column value
+        except KeyError as ke:
+            warnings.warn("Couldn't find column %s in data file" % ke)
+            exit(1)
+    else:
+        commentv=comment
+
+    if value:
+        try:
+            valuev=eval(value)
+        except KeyError as ke:
+            warnings.warn("Couldn't find column %s in data file" % ke)
+            exit(1)
+    else:
+        valuev=value
+    #print(value)
+    #print(valuev)
+    return({'Comment': commentv, 'QuestionId':int(id), 'Value': valuev})
+
+def sdlabrelcalc(string,dat):
+    if dat[string+'relation___1']=="1":
+        return '1'
+    elif dat[string+'relation___2']=="1":
+        return '2'
+    elif dat[string+'relation___3']=="1":
+        return '3'
+    elif dat[string+'relation___4']=="1":
+        return '4'
+    elif dat[string+'relation___5']=="1":
+        return '5'
+    elif dat[string+'relation___6']=="1":
+        return '6'
+    else:
+        return ''
+
+def sdlabsibcode(string,dat):
+    try:
+        x=str(int(dat[string])-1)
+    except ValueError:
+        x=''
+    except TypeError:
+        x=''
+    except KeyError:
+        x=''
+    if x=="-1":
+        return 3
+    elif x=="998":
+        return ''
+    else:
+        return x
---
+++ TranscribeAseba.py
@@ -0,0 +1,121 @@
+import json
+import csv
+import argparse
+import pprint
+import warnings
+import time
+import numpy as np
+import os
+from AsebaParseLib import *
+
+#This script will attempt to convert a csv file from a digital data entry platform (such as Redcap) in to a set of json files readable by the ASEBA scoring program. It depends on AsebaParseScripts.py, and additionally requires a mapping file. CBCL and YSR data output files as well as the mapping file are specified at the command line.
+
+#Debugging: If you get keyerrors it is likely that either a) your mapping file is incorrect or b) your csv files aren't encoded in UTF-8. To fix this, open the csv in excel, save as, and make sure the file type is 'CSV UTF-8'
+
+#Configure Warnings
+def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
+    return ' %s:%s: %s:%s\n' % (os.path.basename(filename), lineno, category.__name__, message)
+
+warnings.formatwarning = warning_on_one_line
+
+#Read Args
+parser = argparse.ArgumentParser()
+parser.add_argument("mapping", help="Specify the CSV file with the study variable mappings.")
+parser.add_argument("cbcl", help="Specify the CSV with the CBCL school age (6-18) form data.")
+parser.add_argument("ysr", help="Specify the CSV with the YSR school age (6-18) form data.")
+args = parser.parse_args()
+# If you don't like arguments you can comment all the lines between 'read args' and here and hard code the file paths here:
+#E.g. :
+#mappingfile=/path/to/your/mappingfile.csv
+mappingfile=args.mapping
+cbclfile=args.cbcl
+ysrfile=args.ysr
+
+#Setup printer (just for debug output)
+pp = pprint.PrettyPrinter(indent=4)
+
+#Read Mapping
+mapping={}
+try:
+    with open(mappingfile, 'r', encoding='utf-8-sig') as csvfile:
+        csvReader=csv.DictReader(csvfile)
+        for rows in csvReader:
+            id = rows['QuestionId']
+            mapping[id] = rows
+except FileNotFoundError:
+    print("Error: Mapping file %s not found" % mappingfile)
+    exit(1)
+
+recordindex=str(mapping['1000']['SourceField'])
+#print("recordindex is %s" % recordindex)
+
+#Read CBCL Data
+cbcldata={}
+try:
+    with open(cbclfile, 'r', encoding='utf-8-sig') as csvfile:
+        csvReader=csv.DictReader(csvfile)
+        for rows in csvReader:
+            #id = scrubadub.clean(rows['subid'], replace_with='identifier')
+            id = rows[recordindex]
+            cbcldata[id] = rows
+except FileNotFoundError:
+    print("Error: CBCL file %s not found" % cbclfile)
+    exit(1)
+except KeyError:
+    print("The mapping file says ID column is '%s', but couldn't find that column in the CBCL file (%s). Adjust question 1000 in the mapping to match the id column of the CBCL file. If they appear to match, there may be a problem with the file encoding (see comments at head of script)" % (recordindex,cbclfile)) 
+
+#pp.pprint(cbcldata['3001'])
+
+#Read YSR Data
+ysrdata={}
+try:
+    with open(ysrfile, 'r', encoding='utf-8-sig') as csvfile:
+        csvReader=csv.DictReader(csvfile)
+        for rows in csvReader:
+            id = rows[recordindex]
+            ysrdata[id] = rows
+except FileNotFoundError:
+    print("Error: YSR file %s not found" % ysrfile)
+    exit(1)
+except KeyError:
+    print("The mapping file says ID column is '%s', but couldn't find that column in the YSR file (%s). Adjust question 1000 in the mapping to match the id column of the CBCL file. If they appear to match, there may be a problem with the file encoding (see comments at head of script)" % (recordindex,ysrfile))
+    exit(1)
+
+#Try to clean out identifiable info.
+
+
+#Make a Subject List
+slist=list(cbcldata.keys())
+ysrslist=list(ysrdata.keys())
+cbclmisslist = np.setdiff1d(ysrslist,slist)
+
+if len(cbclmisslist)!=0:
+    warnings.warn("Records %s are in the ysr data file but not the cbcl data file. They will not be processed because right now we can only get age and gender from the cbcl file. Please add lines to the cbcl file (they do not need to have keyed form data)" % [str(i) + ", " for i in cbclmisslist], UserWarning)
+#print(slist)
+
+try:
+    os.mkdir('output',)
+except OSError as exc:  # Python >2.5
+    if os.path.isdir('output'):
+        print("output directory exists")
+        pass
+    else:
+        raise
+
+for i in slist: #["5010"]:
+    print("processing %s " % i)
+    try: 
+        ysrtemp=ysrdata[i]
+    except KeyError:
+        warnings.warn("Record %s is not in YSR file; youth data will be missing" % i,UserWarning)
+        ysrtemp=None
+    sdict=asebadictformat(mapping,cbcldata[i],ysrtemp)
+    with open(os.path.join('output',i+'.json'), 'w') as fp:
+        json.dump(sdict, fp)
+
+with open(os.path.join('output','[Content_Types].xml'), 'w') as fp:
+    fp.write("""<?xml version="1.0" encoding="utf-8"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="json" ContentType="" /></Types>""")
+#This will print a python dict based on our example json file.
+#with open("9999.json") as json_file:
+#   data = json.load(json_file)
+
